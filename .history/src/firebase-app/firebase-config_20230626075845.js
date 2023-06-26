16c5f94c6e2d7cac732e38210623312bcb583542
Le Khanh <ledinhkhanh2001st@gmail.com>
2023-06-26 16:43:10 +0700

    first commit

---
+++ .history/src/firebase-app/firebase-config_20230626075845.js
@@ -0,0 +1,34 @@
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// import { getAuth } from "firebase/auth";
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+// };
+
+// const app = initializeApp(firebaseConfig);
+// export const db = getFirestore(app);
+// export const auth = getAuth(app);
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// import { getFirestore } from "firebase/firestore";
+// import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyA2IaVtHHgBSofb3zfDPRdPffPwztqUfLI",
+  authDomain: "monkey-blogging-239d4.firebaseapp.com",
+  projectId: "monkey-blogging-239d4",
+  storageBucket: "monkey-blogging-239d4.appspot.com",
+  messagingSenderId: "311216212501",
+  appId: "1:311216212501:web:8f6d003837e57c54eb9c1c",
+  measurementId: "G-CC9HV0DV84"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
